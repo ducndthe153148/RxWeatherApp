92bc23f8f404bfb5305201543fa5c97b96ab47ea
ducndthe153148 <ducndthe153148@fpt.edu.vn>
2022-01-05 21:43:28 +0700

    Fix

--- CallAPI-MVVM-Rx/View/HourlyTableViewCell/CustomerHeaderView.swift
+++ CallAPI-MVVM-Rx/View/HourlyTableViewCell/CustomerHeaderView.swift
@@ -43,32 +43,15 @@ class CustomerHeaderView: UITableViewHeaderFooterView {
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
         print("Ga la ga dang gay 123 ")
-//        collectionView.register(WeatherCollectionViewCell.nib(), forCellWithReuseIdentifier: WeatherCollectionViewCell.identifier)
-//        collectionView.delegate = self
-//        collectionView.dataSource = self
+
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         print("Ga la ga dang gay 456")
-//        collectionView.register(WeatherCollectionViewCell.nib(), forCellWithReuseIdentifier: WeatherCollectionViewCell.identifier)
-//        collectionView.delegate = self
-//        collectionView.dataSource = self
-    }
-    
-}
 
-extension CustomerHeaderView: UICollectionViewDataSource, UICollectionViewDelegate {
-    // Khong chay vao day
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 24
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeatherCollectionViewCell.identifier, for: indexPath) as! WeatherCollectionViewCell
-        cell.timeNow.text = "16"
-        return cell
-    }
 }
 
 extension CustomerHeaderView {
